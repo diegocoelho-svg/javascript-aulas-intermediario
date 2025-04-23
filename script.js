257b24848c5707338ca6f958c1689720acd5822e
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-23 09:10:01 -0300

    Verificando se existe um conteúdo no array

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-let myArray = [
-  "Um texto",
-  10,
-  true,
-  function() {
-    console.log("Função dentro do Array")
-  },
-  {
-    name: "Diego",
-    email: "diego@email.com"
-  }
-]
+let fruits = ["Apple", "Orange", "Banana"]
 
-// Texto.
-console.log(myArray[0])
-console.log(myArray[1])
-console.log(myArray[2])
-myArray[3]()
-console.log(myArray[4].name, myArray[4].email)
-console.log(myArray[4].email)
+// Verifica se um item existe no Array.
+console.log(fruits.includes("Apple"))
+console.log(fruits.includes("Strawberry"))
