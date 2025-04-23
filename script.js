5b0852e14b5fa0770395cd5d5206b084861d735b
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-23 19:54:00 -0300

    for

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// DO WHILE: Repete até que a condição especificada seja VERDADEIRA. No entanto, a instrução será executada pelo menos uma vez antes da condição ser verificada.
+/* FOR repete até que a condição especificada seja FALSA
+  - variável de controle
+  - condição
+  - incremento ou decremento da variável de controle
+*/
 
-let value = 11
+for (i = 0; i < 10; i++) {
+  console.log(i)
+}
 
-do {
-  value++
-  console.log(value)
-} while (value < 10)
-
-console.log("Segue fluxo...")
+// Exemplo de tabuada.
+/*console.log(`7x0 = ${7 * 0}`)
+console.log(`7x0 = ${7 * 1}`)
+console.log(`7x0 = ${7 * 2}`)
+console.log(`7x0 = ${7 * 3}`)
+console.log(`7x0 = ${7 * 4}`)
+console.log(`7x0 = ${7 * 5}`)
+console.log(`7x0 = ${7 * 6}`)
+console.log(`7x0 = ${7 * 7}`)
+console.log(`7x0 = ${7 * 8}`)
+console.log(`7x0 = ${7 * 9}`)
+console.log(`7x0 = ${7 * 10}`)*/
+let number = 7
+for (i = 0; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`)
+}
