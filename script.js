f060ce0171cb55cdd9dadf15fe08a4e260a28084
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-23 08:49:57 -0300

    Convertendo uma string para array

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// Criando Array com construtor
-const newArray = new Array()
-console.log(newArray.length)
+let fullName = "Diego Báu Coelho"
+console.log(fullName)
 
-// Cria o Array com 10 posições vazias.
-const available = new Array(10)
-console.log(available)
+// Cria um Array com os nomes separando pelo espaço.
+console.log(fullName.split(" "))
+
+// Cria um Array com as letras.
+console.log(Array.from(fullName))
+
