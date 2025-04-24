e60e8a42046462c5f441c4ecf5c71514307b44c4
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-23 23:25:47 -0300

    for of

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-/* FOR repete até que a condição especificada seja FALSA
-  - variável de controle
-  - condição
-  - incremento ou decremento da variável de controle
-*/
+// for ... of itera sobre valores de um objeto iterável (Array é um objeto iterável)
+let students = ["Rodrigo", "João", "Amanda"]
 
-for (i = 0; i < 10; i++) {
-  console.log(i)
+for (let student of students) { // Retorna o conteúdo
+  console.log(student)
 }
 
-// Exemplo de tabuada.
-/*console.log(`7x0 = ${7 * 0}`)
-console.log(`7x0 = ${7 * 1}`)
-console.log(`7x0 = ${7 * 2}`)
-console.log(`7x0 = ${7 * 3}`)
-console.log(`7x0 = ${7 * 4}`)
-console.log(`7x0 = ${7 * 5}`)
-console.log(`7x0 = ${7 * 6}`)
-console.log(`7x0 = ${7 * 7}`)
-console.log(`7x0 = ${7 * 8}`)
-console.log(`7x0 = ${7 * 9}`)
-console.log(`7x0 = ${7 * 10}`)*/
-let number = 7
-for (i = 0; i <= 10; i++) {
-  console.log(`${number} x ${i} = ${number * i}`)
+let user = [
+  {
+    name: "Rodrigo",
+    email: "rodrigo@email.com",
+  }
+]
+
+for (let value of user) {
+  console.log(value)
 }
