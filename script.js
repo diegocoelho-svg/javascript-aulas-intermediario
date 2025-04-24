ee3903b8b5041e3dc5f005abd3173a654ff9a43c
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-24 17:27:50 -0300

    toLocaleString

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let date = new Date("2024-07-02T14:00:00")
 
-console.log(date.toLocaleDateString())
-console.log(date.toLocaleTimeString())
+console.log(date.toLocaleString())
+console.log(date.toLocaleString("en"))
 
+// Exibe a data e hora em styles diferentes
+console.log(date.toLocaleString("pt-BR"))
+
+console.log(date.toLocaleString("pt-BR", {
+  dateStyle: "short",
+}))
+
+console.log(date.toLocaleString("pt-BR", {
+  dateStyle: "medium",
+}))
+
+console.log(date.toLocaleString("pt-BR", {
+  dateStyle: "full",
+}))
+
+console.log(date.toLocaleString("pt-BR", {
+  day: "2-digit",
+  month:"2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+}))
+
+let amount = 5
+console.log(amount.toLocaleString("PT-BR", {
+  style:"currency",
+  currency: "BRL",
+}))
