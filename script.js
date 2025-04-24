834dd4c2d9d69101189381729403d50b2e08c6c3
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-24 16:56:19 -0300

    convertendo data para String

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 let date = new Date("July, 3, 2024 14:00:00")
 
-// Formata para o dia sempre ter 2 dígitos
-let day = date.getDate().toString().padStart(2, "0")
-// console.log(day)
+// Converte para String
+console.log(date.toString())
 
-// Formata para o mês sempre ter 2 dígitos
-let month = (date.getMonth() + 1).toString().padStart(2, "0")
-// console.log(month)
+// Retorna apenas a data.
+console.log(date.toDateString())
 
-let year = date.getFullYear()
-let hour = date. getHours()
-let minutes = date.getMinutes()
-
-console.log(`${day}/${month}/${year} ás ${hour}:4${minutes}`)
+// Retorna somente a hora.
+console.log(date.toTimeString())
