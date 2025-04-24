3acec75b2f6fefcb6625a6d7bfcf70217004644c
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-24 16:52:57 -0300

    formatando data e hora

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
 let date = new Date("July, 3, 2024 14:00:00")
 
-console.log(date)
+// Formata para o dia sempre ter 2 dígitos
+let day = date.getDate().toString().padStart(2, "0")
+// console.log(day)
 
-console.log("------------------")
+// Formata para o mês sempre ter 2 dígitos
+let month = (date.getMonth() + 1).toString().padStart(2, "0")
+// console.log(month)
 
-// Modificar o ano.
-date.setFullYear(2030)
-console.log(date)
+let year = date.getFullYear()
+let hour = date. getHours()
+let minutes = date.getMinutes()
 
-console.log("------------------")
-
-// Modificar o mês (Começa com 0)
-date.setMonth(8)
-console.log(date)
-
-console.log("------------------------")
-
-// Modifica a hora 
-date.setHours(18)
-console.log(date)
-
-console.log("------------------------")
-
-date.setMinutes(23)
-console.log(date)
-
-console.log("------------------------")
-
-date.setSeconds(30)
-console.log(date)
+console.log(`${day}/${month}/${year} ás ${hour}:4${minutes}`)
