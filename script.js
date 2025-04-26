5c518dfa27d1ad2842be43e0c29dcce6b3e43d61
Diego Coelho <diegocoelhoaprova@gmail.com>
2025-04-26 16:53:38 -0300

    Como utilizar classes pra lidar com exceções

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const address = {
-  city: "São Paulo",
-  country: "Brazil",
+let obj = []
+let index = 350
+
+try {
+  // obj.execute()
+  if(!obj.includes(17)) {
+    throw new TypeError("O número 17 não está disponível.")
+  }
+
+  if (index > 99) {
+    throw new RangeError("Número está fora do intervalo. Escolha um número de 0 a 99")
+  }
+  
+} catch (error) {
+  if(error instanceof TypeError) {
+    console.log(error)
+  } else if(error instanceof RangeError) {
+    console.log(error.message)
+  } else {
+    console.log("Não foi possível realizar a ação")
+  }
+
+  // console.log(error)
 }
-
-console.log(address)
-
-const users = ["Rodrigo", "Joao"]
-console.log(users)
-
-const username = "Diego Gonçalves"
-console.log(username.__proto__)
